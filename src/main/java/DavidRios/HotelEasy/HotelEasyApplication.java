d44d9c8542178fbab9903390ce1d4890d9672daa
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-08 17:42:37 +0100

    Initial commit

---
+++ src/main/java/DavidRios/HotelEasy/HotelEasyApplication.java
@@ -0,0 +1,13 @@
+package DavidRios.HotelEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DavidRios/HotelEasy/HotelEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package DavidRios.HotelEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
