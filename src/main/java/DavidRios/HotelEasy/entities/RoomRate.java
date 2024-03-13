a3a8744af83bc83bd6541cdf003b6a3387e73160
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-13 13:17:43 +0100

    Adds RoomRate entity and establishes relation with RoomType.

---
+++ src/main/java/DavidRios/HotelEasy/entities/RoomRate.java
@@ -0,0 +1,35 @@
+package DavidRios.HotelEasy.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "room_rates")
+@NoArgsConstructor
+@Getter
+@Setter
+public class RoomRate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
+    private BigInteger id;
+    @ManyToOne
+    @JoinColumn(name = "room_type_id")
+    private RoomType roomType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double rate;
+
+    public RoomRate(RoomType roomType, LocalDate startDate, LocalDate endDate, double rate) {
+        this.roomType = roomType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rate = rate;
+    }
+}
--- src/main/java/DavidRios/HotelEasy/entities/RoomType.java
+++ src/main/java/DavidRios/HotelEasy/entities/RoomType.java
@@ -1,11 +1,14 @@
 package DavidRios.HotelEasy.entities;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "room_types")
@@ -15,10 +18,17 @@ import java.math.BigInteger;
 public class RoomType {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
     private BigInteger id;
     private String roomType;
+    @OneToMany(mappedBy = "roomType")
+    private List<RoomRate> roomRates = new ArrayList<>();
 
     public RoomType(String roomType) {
         this.roomType = roomType;
+    }
+
+    public void setRoomRate (RoomRate roomRate) {
+      this.roomRates.add(roomRate);
     }
 }
