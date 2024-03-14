345caca0a015bce551381e47d142bfa7753d11b4
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-14 15:23:15 +0100

    Adds query to StaffRepo.

--- src/main/java/DavidRios/HotelEasy/repositories/StaffRepo.java
+++ src/main/java/DavidRios/HotelEasy/repositories/StaffRepo.java
@@ -1,10 +1,13 @@
 package DavidRios.HotelEasy.repositories;
 
+import DavidRios.HotelEasy.entities.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface StaffRepo extends JpaRepository<StaffRepo, UUID> {
+public interface StaffRepo extends JpaRepository<Staff, UUID> {
+    Optional<Staff> findByEmail(String email);
 }
